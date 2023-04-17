dba8594b9c2d5de9511acbfb4895be5c15ef0e67
Fleisher <42551611+fleisher@users.noreply.github.com>
2023-04-18 00:34:45 +0600

    Valid parentheses

--- src/main/java/validparentheses/ValidParentheses.java
+++ src/main/java/validparentheses/ValidParentheses.java
@@ -3,11 +3,37 @@ package validparentheses;
 public class ValidParentheses {
 
     public static void main(String[] args) {
-        System.out.println(isValid(""));
+        String s = "[";
+
+        System.out.println(isValid(s));
     }
 
     public static boolean isValid(String s) {
 
-        return true;
+        char[] b = new char[s.length() / 2 + 1];
+        int i = -1;
+
+        for (char sim : s.toCharArray()) {
+            switch (sim) {
+                case ')': {
+                    if (!(i >= 0 && b[i--] == '(')) { return false; }
+                    break;
+                }
+                case '}': {
+                    if (!(i >= 0 && b[i--] == '{')) { return false; }
+                    break;
+                }
+                case ']': {
+                    if (!(i >= 0 && b[i--] == '[')) { return false; }
+                    break;
+                }
+                default: {
+                    if (++i > s.length() / 2) { return false; }
+                    b[i] = sim;
+                }
+            }
+        }
+
+        return i == -1;
     }
 }
